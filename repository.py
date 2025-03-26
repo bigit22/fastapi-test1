d87e1d87b1854e3cbf0bc77f9bced3e8d4477594
bigit22 <koresh.yar@mail.ru>
2025-03-26 15:47:26 +0700

    fix: repo logic

--- repository.py
+++ repository.py
@@ -22,5 +22,5 @@ class TaskRepository:
             query = select(TaskORM)
             result = await session.execute(query)
             task_models = result.scalars().all()
-            task_schemas = [STask(**task_model.__dict__) for task_model in task_models]
+            task_schemas = [STask.model_validate(task_model) for task_model in task_models]
             return task_schemas
